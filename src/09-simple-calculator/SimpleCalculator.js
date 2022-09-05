f82a80df8afeb53b84f4c4dc6f73087e6e0df7a8
José María <josemmazzei@gmail.com>
2022-09-05 10:18:11 -0300

    Add reducer calculator challenge

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,33 +1,97 @@
-const initialState = {}
+import React, { useReducer } from 'react'
 
-function reducer (state, action) {}
+const initialState = { first: 0, second: 0, resut: 0 }
 
-export default function SimpleCalculator () {
+function reducer(state, action) {
+  switch (action.type) {
+    case 'first':
+      return {
+        ...state,
+        first: action.payload,
+      }
+    case 'second':
+      return {
+        ...state,
+        second: action.payload,
+      }
+    case 'increment':
+      return {
+        ...state,
+        result: state.first + state.second,
+      }
+    case 'decrement':
+      return {
+        ...state,
+        result: state.first - state.second,
+      }
+    case 'reset':
+      return {
+        ...state,
+        result: 0,
+      }
+    default:
+      break
+  }
+}
+
+export default function SimpleCalculator() {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div>
       <div>
         <h2>Number 1</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button
+            key={number}
+            onClick={() => {
+              dispatch({ type: 'first', payload: number })
+            }}>
             {number}
-          </button>))}
+          </button>
+        ))}
       </div>
       <div>
         <h2>Number 2</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button
+            onClick={() => {
+              dispatch({ type: 'second', payload: number })
+            }}
+            key={number}>
             {number}
-          </button>))}
+          </button>
+        ))}
       </div>
       <div>
         <h2>Actions</h2>
-        <button>+</button>
-        <button>-</button>
-        <button>c</button>
+        <button
+          onClick={() => {
+            dispatch({
+              type: 'increment',
+            })
+          }}>
+          +
+        </button>
+        <button
+          onClick={() => {
+            dispatch({
+              type: 'decrement',
+            })
+          }}>
+          -
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'reset' })
+          }}>
+          c
+        </button>
       </div>
-      <div><h2>Result:</h2></div>
+      <div>
+        <h2>Result: {state?.result}</h2>
+      </div>
     </div>
   )
 }
