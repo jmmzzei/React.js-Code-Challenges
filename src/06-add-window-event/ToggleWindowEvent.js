6d6bbd9b69ee64b6d92dd7fbd745b1ab64e342c0
José María <josemmazzei@gmail.com>
2022-09-05 09:08:27 -0300

    Add ToggleWindowEvent challenge

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import WindowEvent from './WindowEvent'
 
-export default function ToggleWindowEvent () {
+export default function ToggleWindowEvent() {
   const [windowEvent, setWindowEvent] = useState(false)
+  useEffect(() => {
+    const dblClick = () => alert('ter')
+
+    if (windowEvent) {
+      window.addEventListener('dblclick', dblClick)
+    }
+
+    return () => window.removeEventListener('dblclick', dblClick)
+  }, [windowEvent])
+
   return (
     <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
+      <button onClick={() => setWindowEvent(prevState => !prevState)}>
+        Toggle Window Event
+      </button>
       {windowEvent && <WindowEvent />}
     </div>
   )
