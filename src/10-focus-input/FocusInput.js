f13065a6fe9f7c6726b42d5f7ef096f41b3f6243
José María <josemmazzei@gmail.com>
2022-09-05 10:24:59 -0300

    Add focus input challenge

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,17 @@
+import React, { useRef } from 'react'
+
 export default function FocusInput() {
+  const input = useRef(null)
+
+  React.useEffect(() => {
+    input.current.focus()
+  }, [])
+
   return (
     <div>
-      <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <label htmlFor="focused-input">Focus me on page load!</label>
+      <input ref={input} name="focused-input"></input>
     </div>
   )
 }
+// it can be done more simple with the autoFocus input's attr
