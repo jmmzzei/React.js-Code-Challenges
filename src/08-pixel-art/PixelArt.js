41ce456696fbf0bdb324d5f8dc09fade49b54c6e
José María <josemmazzei@gmail.com>
2022-09-05 09:27:07 -0300

    Add pixel art challenge

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,81 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
-function ColorPicker () {
+const Context = React.createContext('black')
+
+function ColorPicker() {
   const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+  const [, setContext] = useContext(Context)
+
+  const handleClick = color => {
+    setContext(color)
+  }
+
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map(color => (
+        <button
+          key={color}
+          style={{ backgroundColor: color }}
+          onClick={() => handleClick(color)}
+        />
+      ))}
     </div>
   )
 }
 
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
+function Pixel() {
+  const [context] = useContext(Context)
+  const [color, setColor] = React.useState('lightGrey')
+
+  const handleClick = () => {
+    setColor(context)
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      style={{
+        height: '20px',
+        width: '20px',
+        backgroundColor: color,
+        margin: '1px',
+      }}
+    />
+  )
 }
 
-function Pixels () {
+function Pixels() {
   const pixels = []
   for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(10, 1fr)',
+        width: '210px',
+        margin: '0 auto',
+      }}>
       {pixels}
     </div>
   )
 }
 
-export default function PixelArt () {
+export const ContextProvider = ({ children }) => {
+  const [state, setState] = React.useState('black')
+
   return (
-    <div>
-      <ColorPicker />
-      <Pixels />
-    </div>
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+  )
+}
+
+export default function PixelArt() {
+  return (
+    <ContextProvider>
+      <div>
+        <ColorPicker />
+        <Pixels />
+      </div>
+    </ContextProvider>
   )
 }
